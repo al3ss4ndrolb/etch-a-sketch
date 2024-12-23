509fcf92e3bc1e45999878d05b6cf8c8a86bf759
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-23 23:36:22 +0100

    update: finish exercise - only bonus missing

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-const createDivs = () => {
-  for (let i = 0; i < 256; i++) {
-    const newDiv = document.createElement("div");
-    const container = document.querySelector(".container");
-    newDiv.classList.add("grid-cell"); // Adds styling class
-    container.appendChild(newDiv);
+const createDivs = (gridSize) => {
+  // function to create grid dinamically
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
 
+  const cellSize = 500 / gridSize;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    // Generate grid cells
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("grid-cell"); // Adds styling class
+    newDiv.style.width = `${cellSize}px`;
+    newDiv.style.height = `${cellSize}px`;
+
+    // Generate random color
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    newDiv.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    newDiv.style.opacity = "0";
+    // Add hover effect
     newDiv.addEventListener("mouseover", () => {
-      newDiv.style.backgroundColor = "red";
+      let currentOpacity = parseFloat(newDiv.style.opacity);
+      if (currentOpacity < 1) {
+        newDiv.style.opacity = (currentOpacity + 0.1).toString(); // Increase by 0.1
+      }
     });
+    container.appendChild(newDiv);
   }
 };
 
+// Initial grid creation (default 16x16)
+createDivs(16);
+
+// Add event listener to the button
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
-  const userResponse = parseInt(prompt("Enter number of squares:"), 10);
+  const userResponse = parseInt(
+    prompt("Please enter number of squares per side, between 1 and 100:"),
+    10
+  );
+
   if (!isNaN(userResponse) && userResponse <= 100) {
-    document.querySelector(".container").innerHTML = "";
-    createDivs();
+    createDivs(userResponse);
   } else {
-    return "invalid input";
+    alert("Invalid input! Try a number between 1 and 100"); //redirects user
   }
 });
-
-createDivs();
