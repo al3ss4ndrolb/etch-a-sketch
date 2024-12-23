d8b3225341b53dc325c576a2a8c77082ab7f51de
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-23 18:16:03 +0100

    update: add button to input number and center it

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  let userResponse = prompt("Enter number of squares:");
+
+  if (typeof userResponse == "number" && userResponse <= 1000) {
+  } else {
+    return "invalid input";
+  }
+});
+
 const createDivs = () => {
   for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
